e8664a8c9b79322cf89acfcd0e14957def13fefe
GabrielDaZK <gabriel.antunes.correa@escola.pr.gov.br>
2025-05-09 09:42:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function somar(){
+    let num1 = 0;
+    let num2 = 0;
+
+    nu1 = window.prompt('digite um numero: ');
+    num2 - window.prompt('digite outro numero:');
+    
+    let soma = num1+ num2;
+
+    alert(soma);
+
+    let resultado = document.querySelector('dis#resultado');
+}
